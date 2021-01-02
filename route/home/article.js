fc13853fcc68a13e87d5f4718d20397397de0c8d
沈晨玙 <939826879@qq.com>
2021-01-03 02:12:45 +0800

    用户界面
    视频23 新闻界面展示

--- route/home/article.js
+++ route/home/article.js
@@ -1,4 +1,13 @@
-module.exports = (req, res) => {
+const{Article}=require('../../model/article')
+module.exports = async(req, res) => {
+    //接收客户端传递过来的文章id值
+    const id = req.query.id;
+    //根据id查询文章详细信息
+    let article=await  Article.findOne({_id:id}).populate('author').lean();
+
+    // res.send(article);
     // res.send('欢迎来到博客文章详情页面');
-    res.render('home/article.art');
+    res.render('home/article.art',{
+        article:article
+    });
 }
